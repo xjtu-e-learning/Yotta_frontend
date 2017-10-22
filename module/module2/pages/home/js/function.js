0042b7afcd580a55843010beea0107dceeb9b4eb
yuanhao <994303>
2017-10-22 09:51:07 +0800

    知识森林前端和数据管理前端合并

---
+++ module/module2/pages/home/js/function.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $.ajax({
+             type: "GET",
+             url: ip+"/DomainAPI/countClassNum",
+             data: {},
+             dataType: "json",
+             success: function(data){
+                         console.log(data.ClassNum);
+                         $('#ClassNum').text("系统中目前共有"+data.ClassNum+"门课程");
+                      }
+         });
+});
+
+function jumpClass(){
+        console.log("success");
+    }
+
+
+
+function tianjiaClass(){
+    var newclass=$("#newclass").val();
+    console.log(newclass);
+
+    $.ajax({
+             type: "GET",
+             url: ip+"/DomainAPI/createClass",
+             data: {ClassName:$("input[name='ClassName']").val()},
+             dataType: "json",
+             success: function(data){
+                         console.log(data);
+                         alert(data.success);
+                      }
+         });
+}
+
+
+function updataClassName(){
+    $.ajax({
+             type: "GET",
+             url: ip+"/DomainAPI/updateClassName",
+             data: {ClassName:nowOperateClass,NewClassName:$("input[name='NewClassName']").val()},
+             dataType: "json",
+             async:false,
+             success: function(data){
+                         console.log(data);
+                         alert(data.success);
+                         
+                      }
+         });
+}
+
+
+
