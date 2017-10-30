3d3099e200e0fe764f8511c44cdde6b58b95ed39
yuanhao <994303>
2017-10-30 21:50:59 +0800

    修改ip

--- module/module2/dist/js/shileiUtils.js
+++ module/module2/dist/js/shileiUtils.js
@@ -1,7 +1,7 @@
 /**
  * 读取配置文件
  */
-var ip = "http://yotta.xjtushilei.com:9218/Yotta";  // 服务器域名(ip) + http端口(9218)
+var ip = "http://202.117.54.39:8080/Yotta";  // 服务器域名(ip) + http端口(9218)
 
 // setCookie("name","hayden");
 // alert(getCookie("name"));
